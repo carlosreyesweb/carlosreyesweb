d610950006ce918d7915ee61d17127d985b9e2ab
Carlos Reyes <cgrt17062011@gmail.com>
2024-02-18 20:59:55 -0400

    chore: add Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
+import { clientEnvironment } from "@/config/environment"
 import { SEO } from "@/config/seo"
 import { GlobalLayout } from "@/layouts/global"
 import { roboto, robotoMono } from "@/styles/fonts"
 import "@/styles/globals.css"
+import { GoogleAnalytics } from "@next/third-parties/google"
 import { DefaultSeo } from "next-seo"
 import type { AppProps } from "next/app"
 
@@ -18,6 +20,11 @@ export default function App({ Component, pageProps }: AppProps) {
       <GlobalLayout>
         <Component {...pageProps} />
       </GlobalLayout>
+      {clientEnvironment.NEXT_PUBLIC_GA_MEASUREMENT_ID ? (
+        <GoogleAnalytics
+          gaId={clientEnvironment.NEXT_PUBLIC_GA_MEASUREMENT_ID}
+        />
+      ) : null}
     </>
   )
 }
