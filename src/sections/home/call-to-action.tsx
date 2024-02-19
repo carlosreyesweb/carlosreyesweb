03060346641ae596510f5948556bd4fed89bcb6e
Carlos Reyes <cgrt17062011@gmail.com>
2024-02-18 20:00:23 -0400

    chore: fix home page CTA

--- src/sections/home/call-to-action.tsx
+++ src/sections/home/call-to-action.tsx
@@ -4,20 +4,18 @@ import { Paragraph } from "@/components/ui/typography"
 
 export function CallToAction() {
   return (
-    <section className="container bg-gradient-to-t from-background via-background to-border p-[1px]">
-      <div className="space-y-8 bg-background py-20 text-center">
-        <SectionTitle>
-          Let&apos;s Build Something Amazing Together.
-          <br />
-          <span className="text-primary">Get in Touch!</span>
-        </SectionTitle>
-        <Paragraph className="mx-auto max-w-4xl">
-          Ready to discuss your project and see how I can contribute? Drop me a
-          line. I&apos;m excited to hear from you and turn your vision into
-          reality.
-        </Paragraph>
-        <LetsChatButton />
-      </div>
+    <section className="container space-y-8 pb-40 pt-20 text-center">
+      <SectionTitle>
+        Let&apos;s Build Something Amazing Together.
+        <br />
+        <span className="text-primary">Get in Touch!</span>
+      </SectionTitle>
+      <Paragraph className="mx-auto max-w-4xl">
+        Ready to discuss your project and see how I can contribute? Drop me a
+        line. I&apos;m excited to hear from you and turn your vision into
+        reality.
+      </Paragraph>
+      <LetsChatButton />
     </section>
   )
 }
