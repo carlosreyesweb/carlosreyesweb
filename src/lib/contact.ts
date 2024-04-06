fb210a7241b7b7924cdc2a4e71c4a58a5b5fb323
carlosreyesweb <contact@carlosreyesweb.com>
2024-04-06 01:49:14 +0000

    chore: test commit

--- src/lib/contact.ts
+++ src/lib/contact.ts
@@ -4,6 +4,7 @@ import emailjs from "@emailjs/browser"
 
 export function sendMail(data: FormValues, recaptchaToken: string) {
   emailjs.init(clientEnvironment.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY)
+
   return emailjs.send(
     clientEnvironment.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
     clientEnvironment.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
