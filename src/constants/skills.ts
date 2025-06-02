49e3aa2e81fe92302d626d1bcd4a556d6d751a9b
carlosreyesweb <contact@carlosreyesweb.com>
2025-06-01 22:46:27 -0400

    fix JSON-LD

---
+++ src/constants/skills.ts
@@ -0,0 +1,127 @@
+import {
+  AstroPlain,
+  Css3Original,
+  DigitaloceanOriginal,
+  DockerOriginal,
+  ExpressOriginal,
+  FlaskOriginalWordmark,
+  Html5Original,
+  JavascriptOriginal,
+  JiraOriginal,
+  LinuxOriginal,
+  MongodbOriginal,
+  MysqlOriginal,
+  NestjsOriginal,
+  NextjsPlain,
+  NodejsOriginal,
+  PostgresqlOriginal,
+  PrismaOriginal,
+  PythonOriginal,
+  ReactOriginal,
+  TailwindcssOriginal,
+  TrelloOriginal,
+  TypescriptOriginal,
+  VercelOriginal,
+  VitejsOriginal,
+} from "devicons-react"
+
+export const skills = [
+  {
+    name: "Next.js",
+    logo: NextjsPlain,
+    color: "white",
+  },
+  {
+    name: "React.js",
+    logo: ReactOriginal,
+  },
+  {
+    name: "Vite",
+    logo: VitejsOriginal,
+  },
+  {
+    name: "Astro",
+    logo: AstroPlain,
+    color: "white",
+  },
+  {
+    name: "HTML",
+    logo: Html5Original,
+  },
+  {
+    name: "CSS",
+    logo: Css3Original,
+  },
+  {
+    name: "Tailwind CSS",
+    logo: TailwindcssOriginal,
+  },
+  {
+    name: "TypeScript",
+    logo: TypescriptOriginal,
+  },
+  {
+    name: "JavaScript",
+    logo: JavascriptOriginal,
+  },
+  {
+    name: "Flask",
+    logo: FlaskOriginalWordmark,
+    color: "white",
+  },
+  {
+    name: "Python",
+    logo: PythonOriginal,
+  },
+  {
+    name: "Nest.js",
+    logo: NestjsOriginal,
+  },
+  {
+    name: "Express.js",
+    logo: ExpressOriginal,
+    color: "white",
+  },
+  {
+    name: "Node.js",
+    logo: NodejsOriginal,
+  },
+  {
+    name: "Prisma ORM",
+    logo: PrismaOriginal,
+  },
+  {
+    name: "MySQL",
+    logo: MysqlOriginal,
+  },
+  { name: "PostgreSQL", logo: PostgresqlOriginal },
+  {
+    name: "MongoDB",
+    logo: MongodbOriginal,
+  },
+  {
+    name: "Docker",
+    logo: DockerOriginal,
+  },
+  {
+    name: "DigitalOcean",
+    logo: DigitaloceanOriginal,
+  },
+  {
+    name: "Vercel",
+    logo: VercelOriginal,
+    color: "white",
+  },
+  {
+    name: "Linux",
+    logo: LinuxOriginal,
+  },
+  {
+    name: "Jira",
+    logo: JiraOriginal,
+  },
+  {
+    name: "Trello",
+    logo: TrelloOriginal,
+  },
+]
