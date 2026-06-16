f139b5ab907235dc938be34b88871d9344311361
carlosreyesweb <contact@carlosreyesweb.com>
2026-06-16 04:20:47 -0400

    update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-// @ts-check
 import react from "@astrojs/react"
 import sitemap from "@astrojs/sitemap"
 import tailwindcss from "@tailwindcss/vite"
@@ -6,7 +5,6 @@ import { defineConfig } from "astro/config"
 
 export default defineConfig({
   site: "https://www.carlosreyesweb.com",
-  trailingSlash: "never",
   vite: {
     plugins: [tailwindcss()],
   },
